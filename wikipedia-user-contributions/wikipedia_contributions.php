96ef892622f953803116636c581a5c9cb430e58d
Macario James <hellothere@macariojames.com>
2017-10-16 11:36:23 -0500

    Added code for Options page; barebones takes only username

--- wikipedia-user-contributions/wikipedia_contributions.php
+++ wikipedia-user-contributions/wikipedia_contributions.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: Wikipedia User Contributions
  * Description: Grab a Wikipedia's user's contributions list and display it on the page
- * Version: 0.1
+ * Version: 0.2
  * Author: Macario James
  * Author URI: http://macariojames.com
  * License: GPL2
  */
 
-/*  Copyright 2016 Macario James - email : hellothere@macariojames.com
+/*  Copyright 2017 Macario James - email : hellothere@macariojames.com
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -25,25 +25,61 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-$dom_script = 'simple_html_dom.php';
-if(is_readable($dom_script))
-	@include_once($dom_script);
-else echo "Couldn't activate Simple HTML DOM script';
+// Creating the Settings page
+function wikipedia_uc_options_page(){ ?>
+    <div class="wrap">
+    <h1>Wikipedia User Contributions Options</h1>
+    <form method="post" action="options.php">
+        <?php
+            settings_fields("section");
+            do_settings_sections("plugin-options");      
+            submit_button(); 
+        ?>          
+    </form>
+	</div>
+<?php 
+}
 
-function wikipedia_contribution()  {
-	$search_term = "macariojames";
-	$url = "https://en.wikipedia.org/wiki/Special:Contributions/".$search_term;
+function add_wikipedia_uc_menu_item(){
+	add_menu_page("Wikipedia UC", "Wikipedia UC", "manage_options", "theme-panel", "wikipedia_uc_options_page", null, 99);
+}
+
+add_action("admin_menu", "add_wikipedia_uc_menu_item");
+
+function display_wikipedia_username() { ?>
+    <input type="text" name="wikipedia_username" id="wikipedia_username" value="<?php echo get_option('wikipedia_username'); ?>" />
+<?php
+}
+
+function display_theme_panel_fields()
+{
+	add_settings_section("section", "All Settings", null, "plugin-options");
+	
+	add_settings_field("wikipedia_username", "Wikipedia Username", "display_wikipedia_username", "plugin-options", "section");
+
+    register_setting("section", "wikipedia_username");
+}
+
+add_action("admin_init", "display_theme_panel_fields");
+
+$dom_script = 'simple_html_dom.php';
+include($dom_script);
+
+function wikipedia_user_contribution()  {
+	$wu = get_option('wikipedia_username');
+
+	$url = "https://en.wikipedia.org/wiki/Special:Contributions/".$wu;
 	 
 	$html = file_get_html($url);
 
-	$output = "<div class='col-md-10 col-md-offset-1' style='clear: both: margin: 0 auto;'> <h6>Wikipedia Contributions (most recent order)</h6><ol class='wikipedia-contributions'>";
+	$output = "<div class='col-md-12' style='clear: both: margin: 0 auto;'> <h5>Wikipedia Contributions (most recent order)</h5><ol class='wikipedia-contributions'>";
 
 	foreach($html->find('ul[class=mw-contributions-list] li') as $ct) {
 		foreach($ct->find('a') as $a) {
-			//prepends each link's href with wikipedia.org since 
-			//by default wikipedia uses relative paths ~mj
-			$a->href = 'http://en.wikipedia.org' . $a->href;
-			$a->target = '_blank'; // adds attribute to open in new window ~mj
+			// prepends each link's href with wikipedia.org since 
+			// by default wikipedia uses relative paths ~mj
+			$a->href = 'https://en.wikipedia.org' . $a->href;
+			$a->target = '_blank'; // adds attribute to open in new window
 		}
 		$output .= "<li>" . $ct->innertext . "</li>";
 	}
@@ -56,7 +92,7 @@ function wikipedia_contribution()  {
 	 
 }
 
-
-add_shortcode('wiki_contribute', 'wikipedia_contribution');
+// adds a shortcode to display in your page/post easily
+add_shortcode('wuc', 'wikipedia_user_contribution');
 
 ?>
